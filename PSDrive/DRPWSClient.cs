3e1408d46e80cb95c7c84f638e9e799434331e77
adhdtech <37223790+adhdtech@users.noreply.github.com>
2019-05-02 16:28:51 -0500

    Restructured .NET module

--- PSDrive/DRPWSClient.cs
+++ PSDrive/DRPWSClient.cs
@@ -7,78 +7,232 @@ using Newtonsoft.Json.Linq;
 
 namespace ADHDTech.DRP
 {
-    public class DRPClient
+    public class DRP_WebsocketConn : WebSocketSharp.WebSocket
     {
-        public WebSocketSharp.WebSocket ClientWSConn;
+        public Dictionary<string, Action<object>> ReplyHandlerQueue;
+        public Dictionary<string, Action<object>> StreamHandlerQueue;
+        public int TokenNum;
+        public DRP_WebsocketConn(string url, params string[] protocols) : base(url, protocols)
+        {
+            ReplyHandlerQueue = new Dictionary<string, Action<object>>();
+            StreamHandlerQueue = new Dictionary<string, Action<object>>();
+            TokenNum = 1;
+        }
+    }
+    public class DRP_Endpoint
+    {
+        public Dictionary<string, Func<Dictionary<string, object>, DRP_WebsocketConn, string, object>> EndpointCmds;
+        public DRP_Endpoint()
+        {
+        }
+
+        public string GetToken(DRP_WebsocketConn wsConn)
+        {
+            // Generate token
+            //string token = Guid.NewGuid().ToString();
+            int replyToken = wsConn.TokenNum;
+            wsConn.TokenNum++;
+            return replyToken.ToString();
+        }
+
+        public string AddReplyHandler(DRP_WebsocketConn wsConn, Action<object> callback)
+        {
+            string replyToken = GetToken(wsConn);
+            wsConn.ReplyHandlerQueue[replyToken] = callback;
+            return replyToken;
+        }
+
+        public void DeleteReplyHandler(DRP_WebsocketConn wsConn, string token)
+        {
+            wsConn.ReplyHandlerQueue.Remove(token);
+        }
+
+        public string AddStreamHandler(DRP_WebsocketConn wsConn, Action<object> callback)
+        {
+            string replyToken = GetToken(wsConn);
+            wsConn.StreamHandlerQueue[replyToken] = callback;
+            return replyToken;
+        }
+
+        public void DeleteStreamHandler(DRP_WebsocketConn wsConn, string token)
+        {
+            wsConn.StreamHandlerQueue.Remove(token);
+        }
+
+        public void RegisterCmd(string cmd, Func<Dictionary<string, object>, DRP_WebsocketConn, string, object> method)
+        {
+            EndpointCmds[cmd] = method;
+        }
+
+        // Send DRP Cmd
+        public void SendCmd(DRP_WebsocketConn wsConn, string serviceName, string cmd, object @params, Action<object> callback)
+        {
+            // Get token
+            string token = AddReplyHandler(wsConn, callback);
+
+            // Send command
+            DRP_Cmd sendCmd = new DRP_Cmd(cmd, serviceName, token, @params);
+            wsConn.Send(Newtonsoft.Json.JsonConvert.SerializeObject(sendCmd));
+        }
+
+        // Send DRP Cmd and wait for results
+        public JObject SendCmd(DRP_WebsocketConn wsConn, string serviceName, string cmd, object @params)
+        {
+            // Define return object
+            JObject returnObject = null;
+
+            // Define task dummy task to await return
+            Task<object> ReturnDataTask = new Task<object>(() =>
+            {
+                return null;
+            });
+
+            // Define action to execute task
+            Action returnAction = () =>
+            {
+                ReturnDataTask.Start();
+            };
+
+            // Send command, wait up to 30 seconds for return
+            SendCmd(wsConn, serviceName, cmd, @params, data =>
+            {
+                try
+                {
+                    if (data.GetType() == typeof(JObject))
+                    {
+                        JObject returnData = (JObject)data;
+                        returnObject = returnData;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("Error converting message to JObject: " + ex.Message + "\r\n<<<" + data + ">>>");
+                }
+                returnAction.Invoke();
+            });
+
+            // Wait for task to complete
+            ReturnDataTask.Wait(30000);
+
+            // Return Data
+            return returnObject;
+        }
+
+        public void ProcessCmd(DRP_WebsocketConn wsConn, DRP_MsgIn message)
+        {
+            if (message.replytoken != null && message.replytoken.Length > 0)
+            {
+                //thisEndpoint.SendReply(wsConn, message.replytoken, cmdResults.status, cmdResults.output);
+            }
+        }
+        public void ProcessReply(DRP_WebsocketConn wsConn, DRP_MsgIn message)
+        {
+            if (wsConn.ReplyHandlerQueue.ContainsKey(message.token))
+            {
+                // Execute callback
+                wsConn.ReplyHandlerQueue[message.token](message.payload);
+                wsConn.ReplyHandlerQueue.Remove(message.token);
+            }
+            else
+            {
+                // Bad token
+                Console.WriteLine("Received command token with no pending callback -> [{0}]", message.token);
+            }
+        }
+        public void ProcessStream(DRP_WebsocketConn wsConn, DRP_MsgIn message) { }
+
+        public void ReceiveMessage(DRP_WebsocketConn wsConn, EventArgs e)
+        {
+            var thisEndpoint = this;
+            // We received data
+            WebSocketSharp.MessageEventArgs messageArgs = (WebSocketSharp.MessageEventArgs)e;
+
+            // See what we received
+            DRP_MsgIn message = Newtonsoft.Json.JsonConvert.DeserializeObject<DRP_MsgIn>(messageArgs.Data);
+
+            switch (message.type)
+            {
+                case "cmd":
+                    thisEndpoint.ProcessCmd(wsConn, message);
+                    break;
+                case "reply":
+                    thisEndpoint.ProcessReply(wsConn, message);
+                    break;
+                case "stream":
+                    thisEndpoint.ProcessStream(wsConn, message);
+                    break;
+                default:
+                    Console.WriteLine("Invalid message.type; here's the JSON data..." + messageArgs.Data);
+                    break;
+            }
+
+        }
+    }
+
+    public class DRP_Client : DRP_Endpoint
+    {
+        public DRP_WebsocketConn wsConn;
         Dictionary<string, Action<object>> DRPCallbacks = new Dictionary<string, Action<object>>();
         public bool clientConnected = false;
         public bool clientDied = false;
 
-        public DRPClient(BrokerProfile brokerProfile)
+        public DRP_Client(BrokerProfile brokerProfile)
         {
             // Connect to WS
-            ClientWSConn = new WebSocketSharp.WebSocket(brokerProfile.URL, new string[]{"drp"});
-            if (brokerProfile.ProxyAddress != "") {
-                ClientWSConn.SetProxy(brokerProfile.ProxyAddress, brokerProfile.ProxyUser, brokerProfile.ProxyPass);
+            wsConn = new DRP_WebsocketConn(brokerProfile.URL, new string[] { "drp" });
+            if (brokerProfile.ProxyAddress != "")
+            {
+                wsConn.SetProxy(brokerProfile.ProxyAddress, brokerProfile.ProxyUser, brokerProfile.ProxyPass);
             }
             //ClientWSConn.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
 
-            ClientWSConn.OnOpen += StartClientSession;
+            wsConn.OnOpen += (sender, e) =>
+                StartClientSession(wsConn, e);
 
-            ClientWSConn.OnMessage += ProcessMessage;
+            wsConn.OnMessage += (sender, e) =>
+                ReceiveMessage(wsConn, e);
 
-            ClientWSConn.OnError += (sender, e) =>
+            wsConn.OnError += (sender, e) =>
                 Console.WriteLine("Error: " + e.Message);
 
-            ClientWSConn.OnClose += EndClientSession;
+            wsConn.OnClose += EndClientSession;
 
-            ClientWSConn.Connect();
+            wsConn.Connect();
         }
 
         public void CloseSession()
         {
             // Close websocket
-            ClientWSConn.Close();
+            wsConn.Close();
         }
 
-        public void StartClientSession(object sender, EventArgs e)
+        public void StartClientSession(DRP_WebsocketConn wsConn, EventArgs e)
         {
             // We have a connection
             //Console.WriteLine("Session open!");
             clientConnected = true;
         }
 
-        public void ProcessMessage(object sender, EventArgs e)
-        {
-            // We received data
-            WebSocketSharp.MessageEventArgs messageArgs = (WebSocketSharp.MessageEventArgs)e;
-            DRP_Response recvCmd = Newtonsoft.Json.JsonConvert.DeserializeObject<DRP_Response>(messageArgs.Data);
-            if (DRPCallbacks.ContainsKey(recvCmd.token))
-            {
-                // Execute callback
-                DRPCallbacks[recvCmd.token](recvCmd.payload);
-                DRPCallbacks.Remove(recvCmd.token);
-            }
-            else
-            {
-                // Bad token
-                Console.WriteLine("Received command token with no pending callback -> [{0}]", recvCmd.token);
-            }
-        }
-
         public void EndClientSession(object sender, EventArgs e)
         {
             // The session has ended
             WebSocketSharp.CloseEventArgs closeArgs = (WebSocketSharp.CloseEventArgs)e;
-            if (!clientConnected) {
+            if (!clientConnected)
+            {
                 clientDied = true;
             }
             //Console.WriteLine("Close code: '" + closeArgs.Code + "'");
         }
 
+        // Shortcut to execute SendCmd
+        public JObject SendCmd(string cmd, object @params)
+        {
+            return SendCmd(wsConn, null, cmd, @params);
+        }
+
         public async void StartDataGathering()
         {
-            while (ClientWSConn.ReadyState != WebSocketSharp.WebSocketState.Open)
+            while (wsConn.ReadyState != WebSocketSharp.WebSocketState.Open)
             {
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
@@ -101,7 +255,8 @@ namespace ADHDTech.DRP
         // Sample call - Register
         public void GetCmds(Action nextAction)
         {
-            SendDRPCmd("getCmds", null, data => {
+            SendCmd(wsConn, null, "getCmds", null, data =>
+            {
                 Newtonsoft.Json.Linq.JObject returnData = (Newtonsoft.Json.Linq.JObject)data;
                 int fakeStatsRecvdMsgs = (int)returnData["fakestats"]["ReceivedMessages"];
                 Console.WriteLine("FakeStats Received Messages: [{0}]", fakeStatsRecvdMsgs);
@@ -114,56 +269,6 @@ namespace ADHDTech.DRP
             Console.WriteLine("Done with startup.");
             nextAction?.Invoke();
         }
-
-        // Send DRPCmd and specify callback
-        public void SendDRPCmd(string cmd, object @params, Action<object> callback)
-        {
-            // Generate token
-            string token = Guid.NewGuid().ToString();
-
-            // Queue callback
-            DRPCallbacks[token] = callback;
-
-            // Send command
-            DRP_Cmd sendCmd = new DRP_Cmd(cmd, token, @params);
-            ClientWSConn.Send(Newtonsoft.Json.JsonConvert.SerializeObject(sendCmd));
-        }
-
-        // Send DRP Cmd and wait for results
-        public JObject SendDRPCmd(string cmd, object @params)
-        {
-            // Define return object
-            JObject returnObject = null;
-
-            // Define task dummy task to await return
-            Task<object> ReturnDataTask = new Task<object>(() => {
-                return null;
-            });
-
-            // Define action to execute task
-            Action returnAction = () => {
-                ReturnDataTask.Start();
-            };
-
-            SendDRPCmd(cmd, @params, data => {
-                try
-                {
-                    if (data.GetType() == typeof(JObject)) {
-                        JObject returnData = (JObject)data;
-                        returnObject = returnData;
-                    }
-                } catch (Exception ex) {
-                    Console.Error.WriteLine("Error converting message to JObject: " + ex.Message + "\r\n<<<" + data + ">>>");
-                }
-                returnAction.Invoke();
-            });
-
-            // Wait for task to complete
-            ReturnDataTask.Wait(30000);
-
-            // Return Data
-            return returnObject;
-        }
     }
 
     public class DRP_Cmd
@@ -171,14 +276,16 @@ namespace ADHDTech.DRP
         public string type;
         public string cmd;
         public object @params;
+        public string serviceName;
         public string replytoken;
 
-        public DRP_Cmd(string cmdName, string cmdToken, object sendData)
+        public DRP_Cmd(string cmdName, string serviceName, string cmdToken, object sendData)
         {
-            type = "cmd";
-            cmd = cmdName;
-            @params = sendData;
-            replytoken = cmdToken;
+            this.type = "cmd";
+            this.cmd = cmdName;
+            this.@params = sendData;
+            this.serviceName = serviceName;
+            this.replytoken = cmdToken;
         }
     }
 
@@ -194,6 +301,18 @@ namespace ADHDTech.DRP
             status = inStatus;
             payload = inPayload;
         }
+    }
+
+    public class DRP_MsgIn
+    {
+        public string type;
+        public string cmd;
+        public string serviceName;
+        public object @params;
+        public object payload;
+        public string status;
+        public string token;
+        public string replytoken;
     }
 
     public class Playbook
