62494f6096d78f850008305788cb4e1f05c9e6b0
adhdtech <jpbrown@adhdtech.com>
2019-10-17 10:54:45 -0500

    Updated so that NodeURL will not be declared for non-listening nodes

--- DRP-nodejs/testProvider-NoListener.js
+++ DRP-nodejs/testProvider-NoListener.js
@@ -1,32 +1,10 @@
 'use strict';
 var drpService = require('drp-service');
-var os = require("os");
 
-var protocol = "ws";
-if (process.env.SSL_ENABLED) {
-    protocol = "wss";
-}
-var port = process.env.PORT || 8080;
-var hostname = process.env.HOSTNAME || os.hostname();
 var registryurl = process.env.REGISTRYURL || "ws://localhost:8080";
 
-let drpWSRoute = "";
-
-// Set config
-let myServerConfig = {
-    "NodeURL": `${protocol}://${hostname}:${port}${drpWSRoute}`,
-    "Port": port,
-    "SSLEnabled": process.env.SSL_ENABLED || false,
-    "SSLKeyFile": process.env.SSL_KEYFILE || "",
-    "SSLCrtFile": process.env.SSL_CRTFILE || "",
-    "SSLCrtFilePwd": process.env.SSL_CRTFILEPWD || "",
-    "WebRoot": process.env.WEBROOT || "webroot"
-};
-
-// Create Broker on expressApp
-console.log(`Starting DRP Node`);
-console.log(`DRP Endpoint: ${myServerConfig.NodeURL}`);
-
+// Create Node
+console.log(`Starting DRP Node...`);
 let myNode = new drpService.Node(["Provider"]);
 
 // Declare dummy stream
