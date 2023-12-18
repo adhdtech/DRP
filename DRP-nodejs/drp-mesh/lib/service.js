cc8a4444f9ef7879a4832c4138d777ca76f640a2
Pete Brown <pete.brown@autozone.com>
2023-12-18 11:47:38 -0600

    Removed legacy snap code

--- DRP-nodejs/drp-mesh/lib/service.js
+++ DRP-nodejs/drp-mesh/lib/service.js
@@ -39,12 +39,6 @@ class DRP_Service {
         this.Dependencies = dependencies || [];
         this.Streams = streams || [];
         this.Status = status || 0;
-        this.isCacheable = false;
-        this.lastSnapTime = null;
-        this.snapIsRunning = false;
-        this.snapStartTime = null;
-        this.snapEndTime = null;
-        this.snapEndStatus = null;
     }
 
     /**
@@ -80,106 +74,6 @@ class DRP_Service {
             serviceDefinition.Classes[className] = thisService.Classes[className].GetDefinition();
         }
         return serviceDefinition;
-    }
-
-    InitiateSnap() {
-        let thisService = this;
-        let returnData = {
-            'status': null,
-            'data': null
-        };
-        if (!thisService.isCacheable) {
-            // Not marked as cacheable, don't bother executing RunSnap
-            returnData = {
-                'status': 'SERVICE NOT CACHEABLE',
-                'data': null
-            };
-        } else if (thisService.snapIsRunning) {
-            // Already running, kick back an error
-            returnData = {
-                'status': 'SNAP ALREADY RUNNING',
-                'data': { 'snapStartTime': thisService.snapStartTime }
-            };
-        } else {
-
-            let runSnap = async () => {
-                thisService.snapStartTime = new Date().toISOString();
-                thisService.snapIsRunning = true;
-                thisService.snapEndStatus = null;
-
-                // Run the provider specific snap logic
-                try {
-                    await thisService.RunSnap();
-                    thisService.snapEndMsg = "OK";
-                } catch (ex) {
-                    thisService.snapEndMsg = ex;
-                }
-
-                thisService.snapEndTime = new Date().toISOString();
-                thisService.lastSnapTime = thisService.snapStartTime;
-                thisService.snapIsRunning = false;
-            };
-
-            runSnap();
-
-            // Return output from collector
-            returnData = {
-                'status': 'SNAP INITIATED',
-                'data': { 'snapStartTime': thisService.snapStartTime }
-            };
-        }
-        return returnData;
-    }
-
-    async RunSnap() {
-        // This is a placeholder; derived classes should override this method
-    }
-
-    async ReadClassCacheFromService(className) {
-        let thisService = this;
-        let replyObj = await thisService.DRPNode.ServiceCmd("CacheManager", "readClassCache", { "serviceName": thisService.serviceName, "className": className });
-        if (replyObj.err) {
-            thisService.DRPNode.log("Could not read cached objects for " + thisService.serviceName + "\\" + className + " -> " + replyObj.err);
-            thisService.Classes[className].records = {};
-            thisService.Classes[className].loadedCache = false;
-        } else {
-            thisService.lastSnapTime = replyObj.lastSnapTime;
-            for (let objIdx in replyObj.docs) {
-                let classObj = replyObj.docs[objIdx];
-                let classObjPK = classObj['_objPK'];
-                thisService.Classes[className].cache[classObjPK] = classObj;
-            }
-
-            thisService.DRPNode.log("Done reading cached objects for " + thisService.serviceName + "\\" + className);
-        }
-        thisService.Classes[className].loadedCache = true;
-        return null;
-    }
-
-    async WriteClassCacheToService(className, cacheData) {
-        let thisService = this;
-
-        // Reject if no data
-        if (Object.keys(cacheData).length === 0) {
-            thisService.DRPNode.log("No collector records to insert for  " + thisService.serviceName + "/" + className);
-            return null;
-        } else {
-            let replyObj = await thisService.DRPNode.ServiceCmd("CacheManager", "writeClassCache", {
-                "serviceName": thisService.serviceName,
-                "className": className,
-                "cacheData": cacheData,
-                "snapTime": thisService.snapStartTime
-            });
-            return replyObj;
-        }
-    }
-
-    async LoadClassCaches() {
-        let thisService = this;
-        let classNames = Object.keys(thisService.Classes);
-        for (let i = 0; i < classNames.length; i++) {
-            await thisService.ReadClassCacheFromService(classNames[i]);
-        }
     }
 
     /**
