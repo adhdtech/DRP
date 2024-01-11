aa26b16eb5722a7f056edf6e936096d779712392
Pete Brown <jpbrown@adhdtech.com>
2024-01-11 11:20:50 -0500

    Moved TopicWatch applet definition out of DRPShell into standalone applet

--- DRP-nodejs/webroot/vdmapplets/vdm-app-DRPShell.js
+++ DRP-nodejs/webroot/vdmapplets/vdm-app-DRPShell.js
@@ -145,144 +145,6 @@ class AppletClass extends DRPApplet {
             }
         }
 
-        let watchWindowApplet = {
-            appletName: "TopicWatch",
-            title: "Topic Watch",
-            sizeX: 700,
-            sizeY: 411,
-            vdmSession: myApp.vdmSession,
-            appletClass: class TopicWatchApplet extends DRPApplet {
-                constructor(appletProfile, startupParams) {
-                    super(appletProfile);
-                    let watchApp = this;
-
-                    // Override Title
-                    watchApp.title += ` - ${startupParams.topicName} (${startupParams.scope})`;
-                    if (startupParams.targetNodeID) {
-                        watchApp.title += ` @ ${startupParams.targetNodeID}`;
-                    }
-                    if (startupParams.singleInstance) {
-                        watchApp.title += `, single instance`;
-                    }
-
-                    // Prerequisites
-                    watchApp.dependencies = [
-                    ];
-
-                    // Dropdown menu items
-                    watchApp.menu = {
-                        "Stream": {
-                            "Stop": () => {
-                                watchApp.appFuncs.stopStream();
-                            },
-                            "Start": () => {
-                                watchApp.appFuncs.startStream();
-                            }
-                        },
-                        "Output": {
-                            "Pretty print objects": () => {
-                                watchApp.appVars.startupParams.prettyPrint = true;
-                            },
-                            "Objects single line": () => {
-                                watchApp.appVars.startupParams.prettyPrint = false;
-                            }
-                        },
-                        "Format": {
-                            "Timestamp": () => {
-                                watchApp.appVars.startupParams.format = "timestamp";
-                            },
-                            "Bare": () => {
-                                watchApp.appVars.startupParams.format = "bare";
-                            },
-                            "Full": () => {
-                                watchApp.appVars.startupParams.format = "full";
-                            }
-                        }
-                    };
-
-                    watchApp.appFuncs = {
-                        startStream: () => {
-                            let topicName = watchApp.appVars.startupParams.topicName;
-                            let scope = watchApp.appVars.startupParams.scope;
-                            let targetNodeID = watchApp.appVars.startupParams.targetNodeID;
-                            let singleInstance = watchApp.appVars.startupParams.singleInstance;
-                            try {
-                                watchApp.sendCmd_StreamHandler("DRP", "subscribe", { topicName: topicName, scope: scope, targetNodeID: targetNodeID, singleInstance: singleInstance }, (streamData) => {
-                                    let outputTimestamp = true;
-                                    let outputData = streamData.payload.Message;
-                                    switch (watchApp.appVars.startupParams.format) {
-                                        case 'bare':
-                                            outputTimestamp = false;
-                                            break;
-                                        case 'full':
-                                            outputTimestamp = false;
-                                            outputData = streamData.payload;
-                                            break;
-                                        default:
-
-                                    }
-                                    let writeMessage = "";
-                                    if (outputTimestamp) {
-                                        writeMessage += `\x1B[94m[${streamData.payload.TimeStamp}] `;
-                                    }
-                                    if (typeof outputData === "string") {
-                                        writeMessage += `\x1B[97m${streamData.payload.Message}\x1B[0m\r\n`;
-                                    } else {
-                                        if (watchApp.appVars.startupParams.prettyPrint) {
-                                            writeMessage += `\x1B[92m${JSON.stringify(outputData, null, 4).replace(/\r?\n/g, "\r\n")}\x1B[0m\r\n`;
-                                        } else {
-                                            writeMessage += `\x1B[92m${JSON.stringify(outputData)}\x1B[0m\r\n`;
-                                        }
-                                    }
-                                    watchApp.appVars.term.write(writeMessage);
-                                });
-                            } catch (ex) {
-                                console.dir(ex);
-                            }
-                        },
-                        stopStream: () => {
-                            let thisStreamToken = watchApp.streamHandlerTokens.pop();
-                            if (thisStreamToken) {
-                                watchApp.sendCmd("DRP", "unsubscribe", { streamToken: thisStreamToken }, false);
-                                watchApp.vdmSession.drpClient.DeleteReplyHandler(thisStreamToken);
-                            }
-                        }
-                    };
-
-                    watchApp.appVars = {
-                        startupParams: startupParams
-                    };
-
-                    watchApp.recvCmd = {
-                    };
-
-                }
-
-                RunStartup() {
-                    let watchApp = this;
-
-                    watchApp.appVars.termDiv = watchApp.windowParts["data"];
-                    watchApp.appVars.termDiv.style.backgroundColor = "black";
-                    let term = new Terminal();
-                    watchApp.appVars.term = term;
-                    watchApp.appVars.fitaddon = new FitAddon.FitAddon();
-                    term.loadAddon(watchApp.appVars.fitaddon);
-                    term.open(watchApp.appVars.termDiv);
-                    term.setOption('cursorBlink', true);
-                    term.setOption('bellStyle', 'sound');
-                    //term.setOption('fontSize', 12);
-
-                    watchApp.appFuncs.startStream();
-
-                    watchApp.resizeMovingHook = function () {
-                        watchApp.appVars.fitaddon.fit();
-                    };
-
-                    watchApp.appVars.fitaddon.fit();
-                }
-            }
-        }
-
         class drpMethodSwitch {
             constructor(switchName, dataType, description) {
                 this.switchName = switchName;
@@ -472,7 +334,7 @@ class AppletClass extends DRPApplet {
                     null,
                     async function (switchesAndDataString, doPipeOut, pipeDataIn) {
                         let switchesAndData = this.ParseSwitchesAndData(switchesAndDataString);
-                        vdmApp.vdmDesktop.closeWindow(myApp);
+                        vdmApp.vdmDesktop.CloseApplet(myApp);
                     }));
 
                 this.AddMethod(new drpMethod("ls",
@@ -1176,7 +1038,7 @@ class AppletClass extends DRPApplet {
                                     return;
                             }
 
-                            await myApp.vdmDesktop.OpenApp(watchWindowApplet, {
+                            await myApp.vdmDesktop.OpenApplet("TopicWatch", {
                                 topicName: topicName,
                                 scope: scope,
                                 targetNodeID: targetNodeID,
