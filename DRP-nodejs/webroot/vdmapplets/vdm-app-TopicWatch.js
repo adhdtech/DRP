4a5bff16b7aaeedd9b4c69c170e334b71fdd92fb
Pete Brown <jpbrown@adhdtech.com>
2024-01-13 17:38:13 -0500

    Removed appVar and appFunc references

--- DRP-nodejs/webroot/vdmapplets/vdm-app-TopicWatch.js
+++ DRP-nodejs/webroot/vdmapplets/vdm-app-TopicWatch.js
@@ -1,131 +1,125 @@
 class AppletClass extends DRPApplet {
     constructor(appletProfile, startupParams) {
         super(appletProfile);
-        let watchApp = this;
+        let thisApplet = this;
 
-        // Override Title
-        watchApp.title += ` - ${startupParams.topicName} (${startupParams.scope})`;
-        if (startupParams.targetNodeID) {
-            watchApp.title += ` @ ${startupParams.targetNodeID}`;
-        }
-        if (startupParams.singleInstance) {
-            watchApp.title += `, single instance`;
-        }
+        thisApplet.startupParams = startupParams
 
-        // Prerequisites
-        watchApp.dependencies = [
-        ];
+        if (startupParams) {
+            // Override Title
+            thisApplet.title += ` - ${startupParams.topicName} (${startupParams.scope})`;
+            if (startupParams.targetNodeID) {
+                thisApplet.title += ` @ ${startupParams.targetNodeID}`;
+            }
+            if (startupParams.singleInstance) {
+                thisApplet.title += `, single instance`;
+            }
+        }
 
         // Dropdown menu items
-        watchApp.menu = {
+        thisApplet.menu = {
             "Stream": {
                 "Stop": () => {
-                    watchApp.appFuncs.stopStream();
+                    thisApplet.StopStream();
                 },
                 "Start": () => {
-                    watchApp.appFuncs.startStream();
+                    thisApplet.StartStream();
                 }
             },
             "Output": {
                 "Pretty print objects": () => {
-                    watchApp.appVars.startupParams.prettyPrint = true;
+                    thisApplet.startupParams.prettyPrint = true;
                 },
                 "Objects single line": () => {
-                    watchApp.appVars.startupParams.prettyPrint = false;
+                    thisApplet.startupParams.prettyPrint = false;
                 }
             },
             "Format": {
                 "Timestamp": () => {
-                    watchApp.appVars.startupParams.format = "timestamp";
+                    thisApplet.startupParams.format = "timestamp";
                 },
                 "Bare": () => {
-                    watchApp.appVars.startupParams.format = "bare";
+                    thisApplet.startupParams.format = "bare";
                 },
                 "Full": () => {
-                    watchApp.appVars.startupParams.format = "full";
+                    thisApplet.startupParams.format = "full";
                 }
             }
-        };
-
-        watchApp.appFuncs = {
-            startStream: () => {
-                let topicName = watchApp.appVars.startupParams.topicName;
-                let scope = watchApp.appVars.startupParams.scope;
-                let targetNodeID = watchApp.appVars.startupParams.targetNodeID;
-                let singleInstance = watchApp.appVars.startupParams.singleInstance;
-                try {
-                    watchApp.sendCmd_StreamHandler("DRP", "subscribe", { topicName: topicName, scope: scope, targetNodeID: targetNodeID, singleInstance: singleInstance }, (streamData) => {
-                        let outputTimestamp = true;
-                        let outputData = streamData.payload.Message;
-                        switch (watchApp.appVars.startupParams.format) {
-                            case 'bare':
-                                outputTimestamp = false;
-                                break;
-                            case 'full':
-                                outputTimestamp = false;
-                                outputData = streamData.payload;
-                                break;
-                            default:
-
-                        }
-                        let writeMessage = "";
-                        if (outputTimestamp) {
-                            writeMessage += `\x1B[94m[${streamData.payload.TimeStamp}] `;
-                        }
-                        if (typeof outputData === "string") {
-                            writeMessage += `\x1B[97m${streamData.payload.Message}\x1B[0m\r\n`;
-                        } else {
-                            if (watchApp.appVars.startupParams.prettyPrint) {
-                                writeMessage += `\x1B[92m${JSON.stringify(outputData, null, 4).replace(/\r?\n/g, "\r\n")}\x1B[0m\r\n`;
-                            } else {
-                                writeMessage += `\x1B[92m${JSON.stringify(outputData)}\x1B[0m\r\n`;
-                            }
-                        }
-                        watchApp.appVars.term.write(writeMessage);
-                    });
-                } catch (ex) {
-                    console.dir(ex);
-                }
-            },
-            stopStream: () => {
-                let thisStreamToken = watchApp.streamHandlerTokens.pop();
-                if (thisStreamToken) {
-                    watchApp.sendCmd("DRP", "unsubscribe", { streamToken: thisStreamToken }, false);
-                    watchApp.vdmSession.drpClient.DeleteReplyHandler(thisStreamToken);
-                }
-            }
-        };
-
-        watchApp.appVars = {
-            startupParams: startupParams
-        };
-
-        watchApp.recvCmd = {
         };
 
     }
 
     RunStartup() {
-        let watchApp = this;
+        let thisApplet = this;
 
-        watchApp.appVars.termDiv = watchApp.windowParts["data"];
-        watchApp.appVars.termDiv.style.backgroundColor = "black";
+        thisApplet.termDiv = thisApplet.windowParts["data"];
+        thisApplet.termDiv.style.backgroundColor = "black";
         let term = new Terminal();
-        watchApp.appVars.term = term;
-        watchApp.appVars.fitaddon = new FitAddon.FitAddon();
-        term.loadAddon(watchApp.appVars.fitaddon);
-        term.open(watchApp.appVars.termDiv);
+        thisApplet.term = term;
+        thisApplet.fitaddon = new FitAddon.FitAddon();
+        term.loadAddon(thisApplet.fitaddon);
+        term.open(thisApplet.termDiv);
         term.setOption('cursorBlink', true);
         term.setOption('bellStyle', 'sound');
         //term.setOption('fontSize', 12);
 
-        watchApp.appFuncs.startStream();
+        thisApplet.StartStream();
 
-        watchApp.resizeMovingHook = function () {
-            watchApp.appVars.fitaddon.fit();
+        thisApplet.resizeMovingHook = function () {
+            thisApplet.fitaddon.fit();
         };
 
-        watchApp.appVars.fitaddon.fit();
+        thisApplet.fitaddon.fit();
+    }
+
+    StartStream() {
+        let thisApplet = this;
+        let topicName = thisApplet.startupParams.topicName;
+        let scope = thisApplet.startupParams.scope;
+        let targetNodeID = thisApplet.startupParams.targetNodeID;
+        let singleInstance = thisApplet.startupParams.singleInstance;
+        try {
+            thisApplet.sendCmd_StreamHandler("DRP", "subscribe", { topicName: topicName, scope: scope, targetNodeID: targetNodeID, singleInstance: singleInstance }, (streamData) => {
+                let outputTimestamp = true;
+                let outputData = streamData.payload.Message;
+                switch (thisApplet.startupParams.format) {
+                    case 'bare':
+                        outputTimestamp = false;
+                        break;
+                    case 'full':
+                        outputTimestamp = false;
+                        outputData = streamData.payload;
+                        break;
+                    default:
+
+                }
+                let writeMessage = "";
+                if (outputTimestamp) {
+                    writeMessage += `\x1B[94m[${streamData.payload.TimeStamp}] `;
+                }
+                if (typeof outputData === "string") {
+                    writeMessage += `\x1B[97m${streamData.payload.Message}\x1B[0m\r\n`;
+                } else {
+                    if (thisApplet.startupParams.prettyPrint) {
+                        writeMessage += `\x1B[92m${JSON.stringify(outputData, null, 4).replace(/\r?\n/g, "\r\n")}\x1B[0m\r\n`;
+                    } else {
+                        writeMessage += `\x1B[92m${JSON.stringify(outputData)}\x1B[0m\r\n`;
+                    }
+                }
+                thisApplet.term.write(writeMessage);
+            });
+        } catch (ex) {
+            console.dir(ex);
+        }
+    }
+
+    StopStream() {
+        let thisApplet = this;
+        let thisStreamToken = thisApplet.streamHandlerTokens.pop();
+        if (thisStreamToken) {
+            thisApplet.sendCmd("DRP", "unsubscribe", { streamToken: thisStreamToken }, false);
+            thisApplet.vdmDesktop.drpClient.DeleteReplyHandler(thisStreamToken);
+        }
     }
 }
 
@@ -137,8 +131,12 @@ let AppletProfile = {
     "appletIcon": "fa-list-alt",
     "showInMenu": false,
     "preloadDeps": false,
-    "dependencies": []
+    "dependencies": [
+        { "JS": "assets/xterm/lib/xterm.js" },
+        { "JS": "assets/xterm/lib/xterm-addon-fit.js" },
+        { "CSS": "assets/xterm/css/xterm.css" }
+    ]
 }
 
 export { AppletProfile, AppletClass }
-//# sourceURL=vdm-app-CommandTesting.js
+//# sourceURL=vdm-app-TopicWatch.js
